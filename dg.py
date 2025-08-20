09ec8d89dcba0c518e6e9f37005e8fff45402d6c
philex <philex.live@gmail.com>
2025-08-20 05:32:42 +0300

    Cleaned up

--- dg.py
+++ dg.py
@@ -1,20 +1,19 @@
 from enum import Enum
 from abc import ABC
 
+
 class State(ABC):
     pass
-
 
 class Init(State):
     def __init__(self, path):
         self.path = path
 
 class Delimit(State):
-    def __init__(self, file, substr, left, right, tokens):
+    def __init__(self, file, substr,  pointer, tokens):
         self.file = file
         self.substr = substr
-        self.left = left
-        self.right = right
+        self.pointer = pointer
         self.tokens = tokens
 
 class Tokenize(State):
@@ -29,6 +28,7 @@ class Finish(State):
     def __init__(self, msg):
         self.msg = msg
 
+
 class Tk(Enum):
     OPEN_PAREN=0
     CLOSE_PAREN=1
@@ -39,54 +39,51 @@ class Tk(Enum):
     FINISH={'message': ''}
 
 
-def is_separator(s):
-    return s in [' ', '\n', '\t']
-
 def analyze(path):
     state = Init(path)
 
     while True:
         match state:
             case Init():
-                print(state)
                 state = initialize(state)
-                print(state)
             case Delimit():
                 state = delimit(state)
             case Tokenize():
-                print(state)
+                print(state.data)
                 state = Parse(0)
             case Parse():
-                print(state)
                 state = Finish('Finished successful')
             case _:
-                print(state)
                 break
+
 
 def initialize(state):
     file = open(state.path, encoding="utf-8")
 
     if file:
-        return Delimit(file, '', 0, 0, [])
+        return Delimit(file, '', 0, [])
 
     message = ("Finishied unsuccessful, " 
                    "file couldn\'t open")
     return Finish(message)
 
 
+def is_separator(s):
+    return s in [' ', '\n', '\t']
+
+
 def delimit(st):
     c = st.file.read(1)
+    cursor = st.file.tell()
 
     if not is_separator(c):
         st.substr += c
-        st.right = st.file.tell()
 
-    if is_separator(c) and st.left == st.right:
-        st.right = st.file.tell()
-        st.left = st.right
-    elif is_separator(c) and st.left != st.right:
+    if is_separator(c) and st.pointer == cursor-1:
+        st.pointer = cursor
+    elif is_separator(c) and st.pointer != cursor-1:
         st.tokens.append(st.substr)
-        st.left = st.right
+        st.pointer = cursor
         st.substr = ''
 
     if c == '':
@@ -97,3 +94,4 @@ def delimit(st):
 
 
 analyze("res.phyobj")
+analyze("res1.phyobj")
